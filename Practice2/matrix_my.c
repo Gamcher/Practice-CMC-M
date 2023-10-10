1411e76e91b86a50c7939ffba45179bd81eb07d5
Gamcher <maryasovhimself2004@gmail.com>
2023-10-10 22:08:27 +0300

    Practice 2

---
+++ Practice2/matrix_my.c
@@ -0,0 +1,8 @@
+double** in_matrix(int, int);
+void out_matr(double**, int, int);
+double** add_matr(double**, int, int, double**, int, int);
+void muln_matr(double, double**, int, int);
+double** mul_matr(double**, int, int, double**, int, int);
+double det_matr(double**, int);
+double** matr_for_minor1(double**, int, int, int);
+void free_matr(double**, int);
